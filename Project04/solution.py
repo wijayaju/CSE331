90d557059b4001eb330efc21f3547bd8055229ef
Justin <justin@Yudis-MacBook-Pro.local>
2025-03-18 19:37:38 -0400

    added project 4 WIP

--- Project04/solution.py
+++ Project04/solution.py
@@ -32,11 +32,11 @@ class CircularDeque:
         self.capacity: int = capacity
         self.size: int = len(data)
         self.queue: List[T] = [None] * capacity
-        self.back: int = (self.size + front - 1) % self.capacity if data else None
+        self.back: int = (self.size + front - 1) // self.capacity if data else None
         self.front: int = front if data else None
 
         for index, value in enumerate(data):
-            self.queue[(index + front) % capacity] = value
+            self.queue[(index + front) // capacity] = value
 
     def __str__(self) -> str:
         """
@@ -69,51 +69,109 @@ class CircularDeque:
     #
     def __len__(self) -> int:
         """
-        INSERT DOCSTRINGS HERE!
+        Returns the length/size of the circular deque.
+
+        :return: int representing length of the circular deque.
         """
-        pass
+        return self.size
 
     def is_empty(self) -> bool:
         """
-        INSERT DOCSTRINGS HERE!
+        Returns a boolean indicating if the circular deque is empty.
+
+        :return: True if empty, False otherwise.
         """
-        pass
+        if self.size > 0:
+            return False
+        return True
 
     def front_element(self) -> T:
         """
-        INSERT DOCSTRINGS HERE!
+        Returns the first element in the circular deque.
+
+        :return: The first element if it exists, otherwise None.
         """
-        pass
+        return None if self.front is None else self.queue[self.front]
 
     def back_element(self) -> T:
         """
-        INSERT DOCSTRINGS HERE!
+        Returns the last element in the circular deque.
+
+        :return: The last element if it exists, otherwise None.
         """
-        pass
+        return None if self.back is None else self.queue[self.back]
 
     def enqueue(self, value: T, front: bool = True) -> None:
         """
-        INSERT DOCSTRINGS HERE!
+        Add a value to either the front or back of the circular deque based off the parameter front.
+
+        :param value: T: value to add into the circular deque.
+        :param front: where to add value T.
+        :return: None.
         """
-        pass
+        self.size += 1
+        if front:
+            if self.front == 0:
+                self.front = self.size - 1
+                self.queue[self.front] = value
+        else:
+            if self.back == self.capacity:
+                self.back = 0
+                self.queue[self.back] = value
+        if self.size == self.capacity:
+            self.grow()
 
     def dequeue(self, front: bool = True) -> T:
         """
-        INSERT DOCSTRINGS HERE!
+        Remove an item from the queue.
+
+        :param front: Whether to remove the front or back item from the dequeue.
+        :return: Removed item, None if empty.
         """
-        pass
+        value = None
+        if self.is_empty():
+            return value
+
+        if front:
+            value = self.front_element()
+            self.front += 1
+        else:
+            value = self.back_element()
+            self.back -= 1
+        if self.size <= self.capacity // 4 and self.capacity // 4 >= 4:
+            self.shrink()
+        return value
 
     def grow(self) -> None:
         """
-        INSERT DOCSTRINGS HERE!
+        Doubles the capacity of CD by creating a
+        new underlying python list with double the
+        capacity of the old one and copies the values over from the current list.
+
+        :return: None.
         """
-        pass
+        self.capacity *= 2
+        self.front = 0
+        self.back = self.size - 1
+        new_queue: List[T] = [None] * self.capacity
+        for i in range(self.size):
+            new_queue[i] = self.queue[i]
 
     def shrink(self) -> None:
         """
-        INSERT DOCSTRINGS HERE!
+        Cuts the capacity of the queue in half using
+        the same idea as grow. Copy over contents of the
+        old list to a new list with half the capacity.
+
+        :return: None.
         """
-        pass
+        self.capacity //= 2
+        self.front = 0
+        self.back = self.size - 1
+        new_queue: List[T] = [None] * self.capacity
+        for i in range(self.size):
+            new_queue[i] = self.queue[i]
+
 
 
 def find_max_wind_speeds(numbers: List[int], size: int) -> List[int]:
